73414c957f16b2f99dc6bb0c333093d45f702c4f
Philip <psegerfast@gmail.com>
2021-09-15 16:06:10 +0200

    Added youtube player

---
+++ src/components/Player.vue
@@ -0,0 +1,17 @@
+<script setup>
+function play(id) {
+  // calling global variable
+  window.player.loadVideoById(id);
+  window.player.playVideo();
+}
+
+function pause() {
+  window.player.pauseVideo();
+}
+</script>
+
+<template>
+  <button @click="play('DXxeOvvNNwc')">The Black Page #1 on piano</button>
+  <button @click="play('CtkZxnkbjtI')">The Black Page #2 live band</button>
+  <button @click="pause()">Pause</button>
+</template>
